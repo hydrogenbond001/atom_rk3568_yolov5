17babd9201acc2b71334139c013c4e337069bb12
hydrogenbond001 <3101383697@qq.com>
2025-04-02 03:30:14 +0800

    add cross

--- build-linux_RK3568.sh
+++ build-linux_RK3568.sh
@@ -1,29 +1,77 @@
-set -e
+#!/bin/bash
+set -e  # 遇到错误立即退出
+BASE_DIR=$(pwd)  # 记录当前目录
+BUILD_DIR=$BASE_DIR/build  # 设定构建目录
 
-# TARGET_SOC="rk3568/rk3588"
-GCC_COMPILER=aarch64-linux-gnu
+# 显示菜单
+echo "请选择编译方式："
+echo "1. aarch64 本地编译"
+echo "2. 交叉编译 (x86_64 到 aarch64)"
+echo "3. 清理构建目录"
+read -p "请输入选项 (1/2/3): " choice
 
-export LD_LIBRARY_PATH=${TOOL_CHAIN}/lib64:$LD_LIBRARY_PATH
-export CC=${GCC_COMPILER}-gcc
-export CXX=${GCC_COMPILER}-g++
+# 选择对应的架构参数
+# 选择对应的架构参数
+declare -A ARCH_MAP=(
+    [1]="build_aarch64:aarch64:"
+    [2]="build_cross_aarch64:aarch64:-DCMAKE_TOOLCHAIN_FILE=$BASE_DIR/toolchains/aarch64-linux-gnu.toolchain.cmake"
+)
 
-ROOT_PWD=$( cd "$( dirname $0 )" && cd -P "$( dirname "$SOURCE" )" && pwd )
-
-# build
-BUILD_DIR=${ROOT_PWD}/build/build_linux_aarch64
-
-if [ ! -d "${BUILD_DIR}" ]; then
-  mkdir -p ${BUILD_DIR}
+if [[ "$choice" == "3" ]]; then
+    echo "清理构建目录..."
+    rm -rf "$BUILD_DIR"
+    echo "清理完成."
+    exit 0
 fi
 
-cd ${BUILD_DIR}
-cmake ../.. -DCMAKE_SYSTEM_NAME=Linux
-make -j8
-make install
-cd -
+# 获取选择的编译参数
+if [[ -z "${ARCH_MAP[$choice]}" ]]; then
+    echo "无效选项，请重新运行脚本选择正确的选项。"
+    exit 1
+fi
 
-# relu版本
-#cd install/rknn_yolov5_demo_Linux/ && ./rknn_yolov5_demo ./model/RK3588/yolov5s-640-640.rknn ../../720p60hz.mp4
-# 使用摄像头
-# cd install/rknn_yolov5_demo_Linux/ && ./rknn_yolov5_demo ./model/RK3588/yolov5s-640-640.rknn 0
+IFS=':' read -r BUILD_SUBDIR ARCH TARGET_OPTIONS <<< "${ARCH_MAP[$choice]}"
+
+mkdir -p "$BUILD_DIR/$BUILD_SUBDIR"
+cd "$BUILD_DIR/$BUILD_SUBDIR"
+
+# 配置和编译
+cmake $TARGET_OPTIONS "$BASE_DIR"
+make -j4
+
+echo "编译完成，输出位于 $BUILD_DIR/$BUILD_SUBDIR"
+
+
+
+
+
+
+
+
+# # TARGET_SOC="rk3568/rk3588"
+# GCC_COMPILER=aarch64-linux-gnu
+
+# export LD_LIBRARY_PATH=${TOOL_CHAIN}/lib64:$LD_LIBRARY_PATH
+# export CC=${GCC_COMPILER}-gcc
+# export CXX=${GCC_COMPILER}-g++
+
+# ROOT_PWD=$( cd "$( dirname $0 )" && cd -P "$( dirname "$SOURCE" )" && pwd )
+
+# # build
+# BUILD_DIR=${ROOT_PWD}/build
+
+# if [ ! -d "${BUILD_DIR}" ]; then
+#   mkdir -p ${BUILD_DIR}
+# fi
+
+# cd ${BUILD_DIR}
+# cmake .. -DCMAKE_SYSTEM_NAME=Linux -DCMAKE_TOOLCHAIN_FILE=../toolchains/toolchain.cmake
+# make -j8
+# make install
+# cd -
+
+# # relu版本
+# #cd install/rknn_yolov5_demo_Linux/ && ./rknn_yolov5_demo ./model/RK3588/yolov5s-640-640.rknn ../../720p60hz.mp4
+# # 使用摄像头
+# # cd install/rknn_yolov5_demo_Linux/ && ./rknn_yolov5_demo ./model/RK3588/yolov5s-640-640.rknn 0
 
--- src/video.cc
+++ src/video.cc
@@ -1,5 +1,5 @@
-//this is for video detect
-//you can switch them in cmakelist.txt 
+// this is for video detect
+// you can switch them in cmakelist.txt
 
 #include <opencv2/opencv.hpp>
 #include <dlfcn.h>
@@ -13,50 +13,46 @@
 #include "rknn_api.h"
 #include "preprocess.h"
 
-
-//#include <errno.h>
-//#include <wiringPi.h>
-//#include <wiringSerial.h>
-//#include <pthread.h>
-//#include <stdlib.h>
-//int fd ;
-//void uart_init()
+// #include <errno.h>
+// #include <wiringPi.h>
+// #include <wiringSerial.h>
+// #include <pthread.h>
+// #include <stdlib.h>
+// int fd ;
+// void uart_init()
 //{
 
-	//int ret;
+// int ret;
 
-  //if ((fd = serialOpen ("/dev/ttyS0", 115200)) < 0) //打开驱动文件，配置波特率
-	//{
-		//fprintf (stderr, "Unable to open serial device: %s\n", strerror (errno)) ;
-		//return 1 ;
-	//}
- 
-	//if (wiringPiSetup () == -1)
-	//{
-		//fprintf (stdout, "Unable to start wiringPi: %s\n", strerror (errno)) ;
-		//return 1 ;
-	//}
+// if ((fd = serialOpen ("/dev/ttyS0", 115200)) < 0) //打开驱动文件，配置波特率
+//{
+// fprintf (stderr, "Unable to open serial device: %s\n", strerror (errno)) ;
+// return 1 ;
+//}
+
+// if (wiringPiSetup () == -1)
+//{
+// fprintf (stdout, "Unable to start wiringPi: %s\n", strerror (errno)) ;
+// return 1 ;
+//}
 
 //}
 
-int x,y;
+int x, y;
 
 // 发送整数的函数
-//void serialPutNumber(const int fd, int num) {
+// void serialPutNumber(const int fd, int num) {
 //    char buffer[32];  // 定义足够大的缓冲区来存储数字字符串
 //    snprintf(buffer, sizeof(buffer), "%d", num);  // 将整数转换为字符串
 //    serialPuts(fd, buffer);  // 通过 serialPuts 发送字符串
 //}
 
 // 发送浮点数的函数
-//void serialPutFloat(const int fd, float num) {
+// void serialPutFloat(const int fd, float num) {
 //    char buffer[32];  // 定义缓冲区来存储浮点数字符串
 //    snprintf(buffer, sizeof(buffer), "%.2f", num);  // 将浮点数转换为字符串，保留两位小数
 //    serialPuts(fd, buffer);  // 通过 serialPuts 发送字符串
 //}
-
-
-
 
 // Function prototypes
 static void dump_tensor_attr(rknn_tensor_attr *attr);
@@ -147,210 +143,208 @@ static int saveFloat(const char *file_name, float *output, int element_size)
   return 0;
 }
 
-
-
-
-
-
 int main(int argc, char **argv)
 {
-    //uart_init();
+  // uart_init();
 
-    int ret;
-    rknn_context ctx;
-    int img_width = 0;
-    int img_height = 0;
-    int img_channel = 0;
-    const float nms_threshold = NMS_THRESH;
-    const float box_conf_threshold = BOX_THRESH;
-    struct timeval start_time, stop_time;
+  int ret;
+  rknn_context ctx;
+  int img_width = 0;
+  int img_height = 0;
+  int img_channel = 0;
+  const float nms_threshold = NMS_THRESH;
+  const float box_conf_threshold = BOX_THRESH;
+  struct timeval start_time, stop_time;
 
-    if (argc <= 2) {
-        std::cerr << "Usage: " << argv[0] << " < modle_address> <media direction>" << std::endl;
-        return -1;
-    }
+  if (argc <= 2)
+  {
+    std::cerr << "Usage: " << argv[0] << " < modle_address> <media direction>" << std::endl;
+    return -1;
+  }
 
-    char *model_name = (char *)argv[1];
-    //char *video_name = (char *)argv[2];//std::stoi(argv[1]);
-    std::string input = argv[2]; // 获取输入参数
-    cv::VideoCapture cap;
+  char *model_name = (char *)argv[1];
+  // char *video_name = (char *)argv[2];//std::stoi(argv[1]);
+  std::string input = argv[2]; // 获取输入参数
+  cv::VideoCapture cap;
 
-    // 判断输入参数是否为数字（摄像头编号）还是文件路径
-    try {
-        int camera_id = std::stoi(input); // 尝试将输入解析为摄像头编号
-        cap.open(camera_id);             // 打开摄像头
-    } catch (...) {
-        cap.open(input); // 如果解析失败，假定输入为视频文件路径
-    }
+  // 判断输入参数是否为数字（摄像头编号）还是文件路径
+  try
+  {
+    int camera_id = std::stoi(input); // 尝试将输入解析为摄像头编号
+    cap.open(camera_id);              // 打开摄像头
+  }
+  catch (...)
+  {
+    cap.open(input); // 如果解析失败，假定输入为视频文件路径
+  }
 
-    // cv::VideoCapture cap(10);
-    // if (!cap.isOpened())
-    // {
-    //     printf("Error opening video capture\n");
-    //     return -1;
-    // }
+  // cv::VideoCapture cap(10);
+  // if (!cap.isOpened())
+  // {
+  //     printf("Error opening video capture\n");
+  //     return -1;
+  // }
 
-    printf("Loading model...\n");
-    int model_data_size = 0;
-    unsigned char *model_data = load_model(model_name, &model_data_size);
-    ret = rknn_init(&ctx, model_data, model_data_size, 0, NULL);
+  printf("Loading model...\n");
+  int model_data_size = 0;
+  unsigned char *model_data = load_model(model_name, &model_data_size);
+  ret = rknn_init(&ctx, model_data, model_data_size, 0, NULL);
+  if (ret < 0)
+  {
+    printf("rknn_init error ret=%d\n", ret);
+    return -1;
+  }
+
+  rknn_input_output_num io_num;
+  ret = rknn_query(ctx, RKNN_QUERY_IN_OUT_NUM, &io_num, sizeof(io_num));
+  if (ret < 0)
+  {
+    printf("rknn_query error ret=%d\n", ret);
+    return -1;
+  }
+
+  rknn_tensor_attr input_attrs[io_num.n_input];
+  memset(input_attrs, 0, sizeof(input_attrs));
+  for (int i = 0; i < io_num.n_input; i++)
+  {
+    input_attrs[i].index = i;
+    ret = rknn_query(ctx, RKNN_QUERY_INPUT_ATTR, &(input_attrs[i]), sizeof(rknn_tensor_attr));
     if (ret < 0)
     {
-        printf("rknn_init error ret=%d\n", ret);
-        return -1;
+      printf("rknn_query error ret=%d\n", ret);
+      return -1;
     }
+    dump_tensor_attr(&(input_attrs[i]));
+  }
 
-    rknn_input_output_num io_num;
-    ret = rknn_query(ctx, RKNN_QUERY_IN_OUT_NUM, &io_num, sizeof(io_num));
-    if (ret < 0)
+  rknn_tensor_attr output_attrs[io_num.n_output];
+  memset(output_attrs, 0, sizeof(output_attrs));
+  for (int i = 0; i < io_num.n_output; i++)
+  {
+    output_attrs[i].index = i;
+    ret = rknn_query(ctx, RKNN_QUERY_OUTPUT_ATTR, &(output_attrs[i]), sizeof(rknn_tensor_attr));
+    dump_tensor_attr(&(output_attrs[i]));
+  }
+
+  int channel = 3;
+  int width = 0;
+  int height = 0;
+  if (input_attrs[0].fmt == RKNN_TENSOR_NCHW)
+  {
+    channel = input_attrs[0].dims[1];
+    height = input_attrs[0].dims[2];
+    width = input_attrs[0].dims[3];
+  }
+  else
+  {
+    height = input_attrs[0].dims[1];
+    width = input_attrs[0].dims[2];
+    channel = input_attrs[0].dims[3];
+  }
+
+  rknn_input inputs[1];
+  memset(inputs, 0, sizeof(inputs));
+  inputs[0].index = 0;
+  inputs[0].type = RKNN_TENSOR_UINT8;
+  inputs[0].size = width * height * channel;
+  inputs[0].fmt = RKNN_TENSOR_NHWC;
+  inputs[0].pass_through = 0;
+
+  cv::Mat img;
+  while (true)
+  {
+    cap >> img;
+    if (img.empty())
     {
-        printf("rknn_query error ret=%d\n", ret);
-        return -1;
+      printf("Empty frame\n");
+      break;
     }
 
-    rknn_tensor_attr input_attrs[io_num.n_input];
-    memset(input_attrs, 0, sizeof(input_attrs));
-    for (int i = 0; i < io_num.n_input; i++)
-    {
-        input_attrs[i].index = i;
-        ret = rknn_query(ctx, RKNN_QUERY_INPUT_ATTR, &(input_attrs[i]), sizeof(rknn_tensor_attr));
-        if (ret < 0)
-        {
-            printf("rknn_query error ret=%d\n", ret);
-            return -1;
-        }
-        dump_tensor_attr(&(input_attrs[i]));
-    }
+    // Convert to RGB
+    cv::cvtColor(img, img, cv::COLOR_BGR2RGB);
 
-    rknn_tensor_attr output_attrs[io_num.n_output];
-    memset(output_attrs, 0, sizeof(output_attrs));
+    // Resize image if needed
+    cv::Mat resized_img;
+    cv::Size target_size(width, height);
+    float scale_w = (float)target_size.width / img.cols;
+    float scale_h = (float)target_size.height / img.rows;
+    float min_scale = std::min(scale_w, scale_h);
+    BOX_RECT pads;
+    memset(&pads, 0, sizeof(BOX_RECT));
+    letterbox(img, resized_img, pads, min_scale, target_size);
+
+    // Set input data
+    inputs[0].buf = resized_img.data;
+    gettimeofday(&start_time, NULL);
+    rknn_inputs_set(ctx, io_num.n_input, inputs);
+
+    // Run inference
+    rknn_output outputs[io_num.n_output];
+    memset(outputs, 0, sizeof(outputs));
     for (int i = 0; i < io_num.n_output; i++)
     {
-        output_attrs[i].index = i;
-        ret = rknn_query(ctx, RKNN_QUERY_OUTPUT_ATTR, &(output_attrs[i]), sizeof(rknn_tensor_attr));
-        dump_tensor_attr(&(output_attrs[i]));
+      outputs[i].want_float = 0;
     }
+    ret = rknn_run(ctx, NULL);
+    ret = rknn_outputs_get(ctx, io_num.n_output, outputs, NULL);
+    gettimeofday(&stop_time, NULL);
+    printf("Inference time: %f ms\n", (__get_us(stop_time) - __get_us(start_time)) / 1000);
 
-    int channel = 3;
-    int width = 0;
-    int height = 0;
-    if (input_attrs[0].fmt == RKNN_TENSOR_NCHW)
+    // Post-process
+    detect_result_group_t detect_result_group;
+    std::vector<float> out_scales;
+    std::vector<int32_t> out_zps;
+    for (int i = 0; i < io_num.n_output; ++i)
     {
-        channel = input_attrs[0].dims[1];
-        height = input_attrs[0].dims[2];
-        width = input_attrs[0].dims[3];
+      out_scales.push_back(output_attrs[i].scale);
+      out_zps.push_back(output_attrs[i].zp);
     }
-    else
+    post_process((int8_t *)outputs[0].buf, (int8_t *)outputs[1].buf, (int8_t *)outputs[2].buf, height, width,
+                 box_conf_threshold, nms_threshold, pads, min_scale, min_scale, out_zps, out_scales, &detect_result_group);
+
+    // Draw results
+    for (int i = 0; i < detect_result_group.count; i++)
     {
-        height = input_attrs[0].dims[1];
-        width = input_attrs[0].dims[2];
-        channel = input_attrs[0].dims[3];
+      detect_result_t *det_result = &(detect_result_group.results[i]);
+      char text[256];
+      sprintf(text, "%s %.1f%%", det_result->name, det_result->prop * 100);
+      int x1 = det_result->box.left;
+      int y1 = det_result->box.top;
+      int x2 = det_result->box.right;
+      int y2 = det_result->box.bottom;
+      x = (x1 + x2) / 2;
+      y = (y1 + y2) / 2;
+      cv::rectangle(img, cv::Point(x1, y1), cv::Point(x2, y2), cv::Scalar(0, 0, 255), 3);
+      cv::putText(img, text, cv::Point(x1, y1 + 12), cv::FONT_HERSHEY_SIMPLEX, 0.4, cv::Scalar(255, 255, 255));
     }
+    // show position
 
-    rknn_input inputs[1];
-    memset(inputs, 0, sizeof(inputs));
-    inputs[0].index = 0;
-    inputs[0].type = RKNN_TENSOR_UINT8;
-    inputs[0].size = width * height * channel;
-    inputs[0].fmt = RKNN_TENSOR_NHWC;
-    inputs[0].pass_through = 0;
+    // serialPutNumber (fd,x);
+    // serialPuts(fd,"    ");
+    // serialPutNumber (fd,y);
+    // serialPuts(fd,"\r\n");
 
-    
-    
-    cv::Mat img;
-    while (true)
-    {
-        cap >> img;
-        if (img.empty())
-        {
-            printf("Empty frame\n");
-            break;
-        }
+    // Show image
+    cv::cvtColor(img, img, cv::COLOR_RGB2BGR);
+    cv::imshow("Detection", img);
 
-        // Convert to RGB
-        cv::cvtColor(img, img, cv::COLOR_BGR2RGB);
+    // Release RKNN outputs
+    rknn_outputs_release(ctx, io_num.n_output, outputs);
 
-        // Resize image if needed
-        cv::Mat resized_img;
-        cv::Size target_size(width, height);
-        float scale_w = (float)target_size.width / img.cols;
-        float scale_h = (float)target_size.height / img.rows;
-        float min_scale = std::min(scale_w, scale_h);
-        BOX_RECT pads;
-        memset(&pads, 0, sizeof(BOX_RECT));
-        letterbox(img, resized_img, pads, min_scale, target_size);
+    // Wait for user input to exit
+    if (cv::waitKey(1) >= 0)
+      break;
 
-        // Set input data
-        inputs[0].buf = resized_img.data;
-        gettimeofday(&start_time, NULL);
-        rknn_inputs_set(ctx, io_num.n_input, inputs);
+    // Free the resized image memory
+    resized_img.release();
+  }
 
-        // Run inference
-        rknn_output outputs[io_num.n_output];
-        memset(outputs, 0, sizeof(outputs));
-        for (int i = 0; i < io_num.n_output; i++)
-        {
-            outputs[i].want_float = 0;
-        }
-        ret = rknn_run(ctx, NULL);
-        ret = rknn_outputs_get(ctx, io_num.n_output, outputs, NULL);
-        gettimeofday(&stop_time, NULL);
-        printf("Inference time: %f ms\n", (__get_us(stop_time) - __get_us(start_time)) / 1000);
+  // Cleanup
+  cap.release();
+  rknn_destroy(ctx);
+  cv::destroyAllWindows();
+  free(model_data);
+  deinitPostProcess(); // Release post-process resources
 
-        // Post-process
-        detect_result_group_t detect_result_group;
-        std::vector<float> out_scales;
-        std::vector<int32_t> out_zps;
-        for (int i = 0; i < io_num.n_output; ++i)
-        {
-            out_scales.push_back(output_attrs[i].scale);
-            out_zps.push_back(output_attrs[i].zp);
-        }
-        post_process((int8_t *)outputs[0].buf, (int8_t *)outputs[1].buf, (int8_t *)outputs[2].buf, height, width,
-                     box_conf_threshold, nms_threshold, pads, min_scale, min_scale, out_zps, out_scales, &detect_result_group);
-
-        // Draw results
-        for (int i = 0; i < detect_result_group.count; i++)
-        {
-            detect_result_t *det_result = &(detect_result_group.results[i]);
-            char text[256];
-            sprintf(text, "%s %.1f%%", det_result->name, det_result->prop * 100);
-            int x1 = det_result->box.left;
-            int y1 = det_result->box.top;
-            int x2 = det_result->box.right;
-            int y2 = det_result->box.bottom;
-            x=(x1+x2)/2;
-            y=(y1+y2)/2;
-            cv::rectangle(img, cv::Point(x1, y1), cv::Point(x2, y2), cv::Scalar(0, 0, 255), 3);
-            cv::putText(img, text, cv::Point(x1, y1 + 12), cv::FONT_HERSHEY_SIMPLEX, 0.4, cv::Scalar(255, 255, 255));
-        }
-        //show position
-
-        //serialPutNumber (fd,x);
-        //serialPuts(fd,"    ");
-        //serialPutNumber (fd,y);
-        //serialPuts(fd,"\r\n");
-        
-        // Show image
-        cv::cvtColor(img, img, cv::COLOR_RGB2BGR);
-        cv::imshow("Detection", img);
-
-        // Release RKNN outputs
-        rknn_outputs_release(ctx, io_num.n_output, outputs);
-
-        // Wait for user input to exit
-        if (cv::waitKey(1) >= 0) break;
-
-        // Free the resized image memory
-        resized_img.release();
-    }
-
-    // Cleanup
-    cap.release();
-    rknn_destroy(ctx);
-    cv::destroyAllWindows();
-    free(model_data);
-    deinitPostProcess(); // Release post-process resources
-
-    return 0;
+  return 0;
 }
