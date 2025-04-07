3164a8ed0c14dd7f3732c944b07750691259a539
hydrogenbond001 <3101383697@qq.com>
2025-04-07 23:26:43 +0800

    uart start

--- src/rk3568_market.cc
+++ src/rk3568_market.cc
@@ -22,11 +22,7 @@
 // #include <wiringSerial.h>
 // #include <pthread.h>
 // #include <stdlib.h>
-
-int start_enable_flag = 0;
 int serial_fd = -1;
-char serial_fd_received;
-// int bytes_read = read(serial_fd, &serial_fd_received, 1);
 int uart_init()
 {
 
@@ -352,28 +348,12 @@ int main(int argc, char **argv)
       printf("Detected %s: %.1f%%\n", det_result->name, det_result->prop * 100);
       // printf("x1: %d, y1: %d, x2: %d, y2: %d\n", x1, y1, x2, y2);
       printf("x: %d, y: %d,\n", x, y);
-
-      int bytes_read = read(serial_fd, &serial_fd_received, 1);
-      if (bytes_read > 0)
-      {                                // 成功接收到数据
-        if (serial_fd_received == 'G') // 收到关时start_enable_flag置0
-        {
-          start_enable_flag = 0;
-        }
-        if (serial_fd_received == 'K') // 收到开时start_enable_flag置1
-        {
-          start_enable_flag = 1;
-        }
-      }
-
       // 发送数据到串口
-      if (start_enable_flag = 1)//收到K开启命令时才使能输出
+      if ((x > 400) && (x < 600))
       {
-        if ((x > 400) && (x < 600))//当物体中心在两条线之中
-        {
-          write(serial_fd, &(det_result->name), 1);
-        }
+        write(serial_fd, &(det_result->name), 1);
       }
+
       // 发送检测到的物体名称和置信度
       // char buffer[128];
 
